db055414807c1d1cbba927234f2bcf386df05b1e
newbiejasper <gaojx1012@gmail.com>
2017-03-23 15:04:33 +0800

    清空我的R文件包

---
+++ C/C/C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C
+//
+//  Created by 高家兴 on 2017/3/19.
+//  Copyright © 2017年 高家兴. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
