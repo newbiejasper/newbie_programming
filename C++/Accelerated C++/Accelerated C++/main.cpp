88648df18f23ad6d308455816111256eb69f596b
newbiejasper <gaojx1012@gmail.com>
2017-12-25 01:36:04 +0800

    C++学习第0章

---
+++ C++/Accelerated C++/Accelerated C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Accelerated C++
+//
+//  Created by 高家兴 on 2017/12/25.
+//  Copyright © 2017年 newbiejasper. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
